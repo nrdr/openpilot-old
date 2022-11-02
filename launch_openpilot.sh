2c0fed4c8c3981bcaf08beb91855d7d83175d5c5
nrdr <81265253+nrdr@users.noreply.github.com>
2022-11-02 11:02:19 -0400

    🛠️ TESTING: Auto-delete live parameters
    Clearing live parameters every 30 seconds seems to make the car more planted.

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+# Clear live parameters every 30 seconds for optimal performance.
+while true; do rm -rf /data/params/d/LiveParameters; sleep 30; done &
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
