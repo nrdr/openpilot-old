ca53ee795031627b16293134bf4054b9aad87715
nrdr <81265253+nrdr@users.noreply.github.com>
2022-11-05 21:04:22 -0400

    ❌ REVERT: Auto-delete live parameters

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 
-# Clear live parameters every 30 seconds for optimal performance.
-while true; do rm -rf /data/params/d/LiveParameters; sleep 30; done &
-
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
